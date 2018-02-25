ad4b991aa804b37ad4cf9eac90be8a820a3b84d4
micontratista <micontratista.app@gmail.com>
2018-02-25 11:52:21 -0300

    modificandocontroller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,19 @@
 class ArticlesController < ApplicationController
 def new
+  @article = Article.new(params[:article])
+
+  @article.save
+  redirect_to @article
+
 end
 
 def create
 render plain: params[:article].inspect
 end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 
 end
